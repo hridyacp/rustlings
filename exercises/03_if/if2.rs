ffe9e1f9a7db8f38c5d13d3f1758778949883e74
Hridya Premkumar <hridyapremkumar@Hridyas-MacBook-Air.local>
2025-07-03 21:49:41 +0530

    if else

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,12 +2,16 @@
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
-    } else {
-        1
+    } else if food == "potato" {
+        "I guess I can eat that."
+    }
+    else {
+        "No thanks!"
     }
 }
 
 fn main() {
+    picky_eater("strawberry");
     // You can optionally experiment here.
 }
 
