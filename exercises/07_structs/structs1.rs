ed3254620a1fe55921f7a3150d8bee6b8505b90a
Hridya Premkumar <hridyapremkumar@Hridyas-MacBook-Air.local>
2025-07-07 21:12:38 +0530

    strings and enums

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -1,9 +1,14 @@
 struct ColorRegularStruct {
+    red: u8,   // Minimum: 0, Maximum: 255
+    green: u8, // Minimum: 0, Maximum: 255
+    blue: u8,  // Minimum: 0, Maximum: 255
     // TODO: Add the fields that the test `regular_structs` expects.
     // What types should the fields have? What are the minimum and maximum values for RGB colors?
 }
 
-struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+struct ColorTupleStruct(
+   u8,u8,u8
+/* TODO: Add the fields that the test `tuple_structs` expects */);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -20,7 +25,11 @@ mod tests {
     fn regular_structs() {
         // TODO: Instantiate a regular struct.
         // let green =
-
+let green =ColorRegularStruct{
+    red:0,
+    green:255,
+    blue:0
+};
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
         assert_eq!(green.blue, 0);
@@ -30,7 +39,7 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct.
         // let green =
-
+let green=ColorTupleStruct(0,255,0);
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -40,6 +49,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
         // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
